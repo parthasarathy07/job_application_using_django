6e474df4ac80fac7aba3c02559644c2e71a227b0
parthasarathy ak <parthasarathy3737@gmail.com>
2025-10-31 07:55:46 +0530

    feat: Add recent questions on dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from jobs.models import Job
 
 def dashboardview(request):
-    return render(request, 'dashboard/dashboard.html')
+    jobList = Job.objects.order_by("-posted_date")[:5]
+    context = {"jobs": jobList}
+    return render(request, 'dashboard/dashboard.html', context)
