31a3e6d918cdae3ee0bcb3eb8a4c0256e1793800
parthasarathy ak <parthasarathy3737@gmail.com>
2025-11-02 19:13:12 +0530

    revert: removed space

--- company/models.py
+++ company/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models import Avg
 
 class Company(models.Model):
     name = models.CharField(max_length=200)
