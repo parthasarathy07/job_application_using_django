4319bdc61060cd09e61e8b171bdaabd268f4eee6
parthasarathy ak <parthasarathy3737@gmail.com>
2025-11-05 23:10:24 +0530

    feat: Add admin.py for all models

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Application
 
-# Register your models here.
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'job', 'applied_date')
+    list_filter = ('applied_date', 'job')
+    search_fields = ('user__username', 'user__email', 'job__title')
+    ordering = ('-applied_date',)
+    date_hierarchy = 'applied_date'
+    list_per_page = 25
+    readonly_fields = ('applied_date',)
+    fieldsets = (
+        ('Applicant Info', {
+            'fields': ('user', 'job')
+        }),
+        ('Metadata', {
+            'fields': ('applied_date',),
+            'classes': ('collapse',),
+        }),
+    )
--- company/admin.py
+++ company/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from .models import Company
 from jobs.models import Company, Job
+from review.models import Review
 
 class JobInline(admin.TabularInline):
     model = Job
     extra = 1
     fields = ('title', 'description', 'location', 'salary',)
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 0
+    readonly_fields = ('created_at',)
+    fields = ('user', 'rating', 'comment', 'created_at')
+    ordering = ('-created_at',)
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
@@ -13,4 +21,4 @@ class CompanyAdmin(admin.ModelAdmin):
     search_fields = ('name', 'main_location')                
     list_filter = ('main_location',)                     
     ordering = ('name',)
-    inlines = [JobInline]                                
+    inlines = [JobInline,ReviewInline]                                
